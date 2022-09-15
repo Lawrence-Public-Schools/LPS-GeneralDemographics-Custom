d2fe859149c30399963cf0594697f491c222b3fa
Logan-Arias-LPS <71857092+Logan-Arias-LPS@users.noreply.github.com>
2022-09-15 17:14:08 -0400

    Update LPSGDC.js
    remove trim code

--- WEB_ROOT/admin/javascript/LPSGDC.js
+++ WEB_ROOT/admin/javascript/LPSGDC.js
@@ -4,7 +4,6 @@ $j(document).ready(function() {
   if ( $j(".collapseHeader").length < 1 ) { makeCollapsible() }
 
   $j( "#LPS-GDCustomhiddentable" ).remove(); /* Remove hidden LPSGDC templates */
-
   
   window.setTimeout(addLegalandDOB, 3000);
 
